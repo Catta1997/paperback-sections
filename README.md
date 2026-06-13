f50f03badce64e1d3e093bf80fab6246dc97b97f
Sinon <34333654+Catta1997@users.noreply.github.com>
2026-06-13 17:31:19 +0200

    format

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # paperback-sections
-
--- src/sections.ts
+++ src/sections.ts
@@ -1,20 +1,20 @@
-import {EditableListForm, getDiscoverySectionsOrder} from "./settings.js"
-import {DiscoverSection} from "@paperback/types";
+import { EditableListForm, getDiscoverySectionsOrder } from "./settings.js";
+import { DiscoverSection } from "@paperback/types";
 
 export class SectionsOrder {
-	sections: { id: string; title: string; }[] = [];
-	discoverySections: Record<string, DiscoverSection> = {};
-	constructor(sections: Record<string, DiscoverSection>) {
-		this.sections = Object.values(sections).map(({ id, title }) => ({
-			id,
-			title
-		}))
-		this.discoverySections = sections
-	}
-	getSettings =  new EditableListForm(this.sections);
-	getSections() {
-		return getDiscoverySectionsOrder(this.sections)
-			.map((key) => this.discoverySections[key.id])
-			.filter(Boolean);
-	}
+  sections: { id: string; title: string }[] = [];
+  discoverySections: Record<string, DiscoverSection> = {};
+  constructor(sections: Record<string, DiscoverSection>) {
+    this.sections = Object.values(sections).map(({ id, title }) => ({
+      id,
+      title,
+    }));
+    this.discoverySections = sections;
+  }
+  getSettings = new EditableListForm(this.sections);
+  getSections() {
+    return getDiscoverySectionsOrder(this.sections)
+      .map((key) => this.discoverySections[key.id])
+      .filter(Boolean);
+  }
 }
